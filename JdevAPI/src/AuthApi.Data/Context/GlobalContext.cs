5d9dafa0b463bf6ab13c08d3fd65545b7777f295
João Girardi <joao.girardi@pontosys.com>
2022-07-26 21:41:28 -0300

    CreateContext / Mappings / Project

---
+++ JdevAPI/src/AuthApi.Data/Context/GlobalContext.cs
@@ -0,0 +1,25 @@
+﻿using AuthApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AuthApi.Data.Context
+{
+    public class GlobalContext : DbContext
+    {
+        public DbSet<Usuario>? Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                         .SelectMany(e => e.GetProperties()
+                             .Where(p => p.ClrType == typeof(string))))
+            {
+                property.SetColumnType("varchar(100)");
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(GlobalContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ JdevAPI/src/AuthApi.Data/Mappings/USuarioMapping.cs
@@ -0,0 +1,20 @@
+﻿using AuthApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthApi.Data.Mappings
+{
+    public class UsuarioMapping : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.ToTable(nameof(Usuario).ToLower());
+
+            builder.HasKey(x => x.Id);
+            builder.Property(p => p.Nome).HasColumnType("varchar(64)").IsRequired();
+            builder.Property(p => p.Senha).HasColumnType("varchar(32)").IsRequired();
+            builder.Property(p => p.Email).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(p => p.Apagado).HasColumnType("bit(1)").HasDefaultValue("0").IsRequired();
+        }
+    }
+}
